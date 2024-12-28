95219be39bbae368262f53c143f15e38577176c1
rabbit-love <rabbit.hole.634226@gmail.com>
2024-12-28 16:25:44 -0500

    fix mobile size and improve title layout

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,12 +1,28 @@
 export function toggleTheme() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
+  const toggleThemBtn = document.getElementById("theme-toggle");
+
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("data-theme", newTheme);
   localStorage.setItem("theme", newTheme);
+
+  if (newTheme === "dark") {
+    toggleThemBtn.textContent = "☀️";
+  } else {
+    toggleThemBtn.textContent = "🌙";
+  }
 }
 
 export function loadTheme() {
+  const toggleThemBtn = document.getElementById("theme-toggle");
+
   const savedTheme = localStorage.getItem("theme");
   const themeToApply = savedTheme ? savedTheme : "light"; // Por defecto, tema claro
   document.documentElement.setAttribute("data-theme", themeToApply);
+
+  if (themeToApply === "dark") {
+    toggleThemBtn.textContent = "☀️";
+  } else {
+    toggleThemBtn.textContent = "🌙";
+  }
 }
